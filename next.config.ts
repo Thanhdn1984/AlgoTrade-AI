fd8094eaeb997b4c2b18a2ff96bbd289df723e52
tien thanh nguyen <nguyentienthanh84@gmail.com>
2025-12-05 00:40:03 +0000

    D:\AlgoTrade-AI>git fetch
    D:\AlgoTrade-AI>git reset --hard 55fd35e
    fata

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
-  distDir: process.env.NODE_ENV === 'production' ? '../out' : '.next',
   typescript: {
     ignoreBuildErrors: true,
   },
